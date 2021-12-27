09dc54bf672acc62de2d2cd778d3d05edbfedde0
Matthew Zito <matthewtzito@gmail.com>
2021-12-27 15:08:46 -0800

    feat: add start, stop, toggle handles
    - add start, stop, toggle handles
    - deprecate revocation

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,34 @@
 import { isObject } from './utils';
 
+interface IEffectOptions {
+	lazy?: boolean;
+}
+
 interface IEffectFunction {
-	(): void;
 	active: boolean;
+	lazy: boolean;
 	handler: () => void;
+	refs: Set<IEffectFunction>[];
 }
 
 type ITargetMap = WeakMap<any, IEffectsMap>;
-type IEffects = Set<IEffectFunction>;
 type IEffectsMap = Map<PropertyKey, IEffects>;
 
-/**
- * A map of revoke handlers. Pass in a proxy reference to retrieve its corresponding handler.
- * @public
- */
-export const revokes = new WeakMap<ReturnType<typeof resonant>, () => void>();
+type IEffects = Set<IEffectFunction>;
 
 /**
  * A map of all tracked targets and their dependencies
+ *
  * @internal
  */
 const targetMap: ITargetMap = new WeakMap();
 
 /**
  * A stack for tracking active effects
+ *
  * @internal
  */
-const effectStack: IEffectFunction[] = [];
+const effectStack: (IEffectFunction | undefined)[] = [];
 
 /**
  * Create a resonant effect. All get / set operations within the handler will be tracked.
@@ -35,25 +37,67 @@ const effectStack: IEffectFunction[] = [];
  *
  * @public
  */
-export function effect(handler: () => void) {
-	const newEffect: IEffectFunction = () => {
-		run(newEffect);
+export function effect(handler: () => void, opts: IEffectOptions = {}) {
+	const { lazy } = opts;
+
+	const newEffect: IEffectFunction = {
+		active: !lazy,
+		lazy: !!lazy,
+		handler: handler,
+		refs: []
 	};
 
-	newEffect.active = true;
-	newEffect.handler = handler;
+	if (!lazy) {
+		run(newEffect);
+	}
 
-	newEffect();
+	return {
+		/**
+		 * Stop an active effect
+		 *
+		 * @public
+		 */
+		stop: () => {
+			stop(newEffect);
+		},
+
+		/**
+		 * Start an inactive effect
+		 *
+		 * @public
+		 */
+		start: () => {
+			if (newEffect.lazy) {
+				newEffect.lazy = false;
+			}
+
+			start(newEffect);
+		},
+
+		/**
+		 * Toggle the effect's active status
+		 *
+		 * @returns A boolean indicating whether the effect is active
+		 *
+		 * @public
+		 */
+		toggle: () => {
+			newEffect.active ? stop(newEffect) : start(newEffect);
+
+			return newEffect.active;
+		}
+	};
 }
 
 /**
- * Make an object resonant. All object properties will be eligible dependencies for an effect.
+ * Make an object resonant.
+ * All object properties will be eligible dependencies for an effect.
  * @param target
  *
  * @public
  */
 export function resonant<T extends Record<any, any>>(target: T) {
-	const { proxy, revoke } = Proxy.revocable<T>(target, {
+	return new Proxy(target, {
 		deleteProperty(target, key) {
 			const hadKey = Reflect.has(target, key);
 			const result = Reflect.deleteProperty(target, key);
@@ -96,16 +140,53 @@ export function resonant<T extends Record<any, any>>(target: T) {
 			return result;
 		}
 	});
+}
 
-	revokes.set(
-		proxy,
-		revokeAndCleanup.bind({
-			revoke,
-			target
-		})
-	);
+/**
+ * Delete all auxillary effect dependencies of a given effect
+ * @param effect
+ *
+ * @internal
+ */
+function cleanup(effect: IEffectFunction) {
+	const { refs } = effect;
 
-	return proxy;
+	if (refs.length) {
+		for (const ref of refs) {
+			ref.delete(effect);
+		}
+	}
+
+	// prevent empty sets from accumulating
+	refs.length = 0;
+}
+
+/**
+ * Remove all effect dependencies if active, then stop the effect
+ * @param effect
+ *
+ * @internal
+ */
+function stop(effect: IEffectFunction) {
+	if (effect.active) {
+		cleanup(effect);
+	}
+
+	effect.active = false;
+}
+
+/**
+ * Start an inactive effect
+ * @param effect
+ *
+ * @internal
+ */
+function start(effect: IEffectFunction) {
+	if (!effect.active) {
+		effect.active = true;
+
+		run(effect);
+	}
 }
 
 /**
@@ -118,41 +199,25 @@ function run(effect: IEffectFunction) {
 	/* istanbul ignore if */
 	if (!effect.active) {
 		effect.handler();
+
 		return;
 	}
 
-	if (!effectStack.includes(effect)) {
+	if (effectStack.indexOf(effect) === -1) {
+		cleanup(effect);
+
 		// using try / catch here allows us to both return the effect handler's return value
 		// and subsequently pop it off the stack
 		try {
 			effectStack.push(effect);
 
 			effect.handler();
+
 			return;
 		} finally {
 			effectStack.pop();
 		}
 	}
-}
-
-/**
- * Revoke the proxy and clear all effects
- *
- * @internal
- */
-function revokeAndCleanup<T>(this: { target: T; revoke: () => void }) {
-	const { target, revoke } = this;
-
-	revoke();
-
-	const effectsMap = targetMap.get(target);
-
-	/* istanbul ignore if */
-	if (!effectsMap) {
-		return;
-	}
-
-	effectsMap.clear();
 }
 
 /**
@@ -180,6 +245,7 @@ function track<T>(target: T, key: PropertyKey) {
 
 		if (!effects.has(activeEffect)) {
 			effects.add(activeEffect);
+			activeEffect.refs.push(effects);
 		}
 	}
 }
@@ -198,7 +264,14 @@ function trigger(target: any, key: PropertyKey) {
 		return;
 	}
 
+	// this is necessary to prevent infinite recursion
+	// if we run `cleanup` *and* run the effects as we iterate `effectsMap`, we'll
+	// end up invoking `run` and end up back here all over again
+	const scheduled = new Set<IEffectFunction>();
+
 	effectsMap.get(key)?.forEach((effect) => {
-		effect();
+		scheduled.add(effect);
 	});
+
+	scheduled.forEach(run);
 }
